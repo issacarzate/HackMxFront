df2b5ccb7a6829fa778dc868274817dff5b65001
Issac Arzate Soto <issac@MacBook-Pro-de-Issac.local>
2018-02-03 13:11:08 -0600

    Entrega q

---
+++ app/src/main/java/com/example/issac/fitnessandroid/Tab2Dietas.java
@@ -0,0 +1,28 @@
+package com.example.issac.fitnessandroid;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * Created by issac on 03/02/18.
+ */
+
+public class Tab2Dietas extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+        return rootView;
+    }
+}
